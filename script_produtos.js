d3fdaf531fdc0f63da875a5abc2d0ef9cb79bc74
Thiago M. Dunga <109359014+GITuserthiago@users.noreply.github.com>
2022-10-21 18:06:27 -0300

    Add files via upload

---
+++ script_produtos.js
@@ -0,0 +1,24 @@
+
+
+
+/* Set the width of the sidebar to 250px (show it) */
+function openNav() {
+    document.getElementById("mySidepanel").style.width = "250px";
+  }
+  
+  /* Set the width of the sidebar to 0 (hide it) */
+  function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+  }
+
+
+/*----------------------------------------------------------------------*/
+
+
+function myFunction(smallimg)
+{
+    var fullimg = document.getElementById("imagebox");
+    fullimg.src = smallimg.src;
+
+
+}
